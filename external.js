297e31e9169cfa2f52421297fc881b3fc6d6ea2b
Hariprasad-0123 <harisavala787@gmail.com>
2026-05-03 11:10:26 +0530

    Add files via upload

---
+++ external.js
@@ -0,0 +1,16 @@
+ num=331;
+ result=num%2==0?"even number":"odd number"
+ console.log(result)
+
+
+ a=10;
+ b='10';
+ c=true;
+ d=1;
+ console.log(c==d);
+ console.log(c==d);
+ console.log(typeof a);
+ console.log(typeof c);
+
+
+ 
